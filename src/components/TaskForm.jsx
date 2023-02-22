51e15f9f949dd37731a66933de592d793d60b244
santiR22 <santiagorodas688@gmail.com>
2023-02-22 14:35:03 -0300

    first commit

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/taskSlice";
+import { v4 as uuid } from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
+
+export const TaskForm = () => {
+  const params = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const tasks = useSelector((state) => state.tasks);
+
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (params.id) {
+      dispatch(editTask(task));
+    } else {
+      dispatch(
+        addTask({
+          ...task,
+          id: uuid(),
+        })
+      );
+    }
+
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
+    }
+  }, [params.id, tasks]);
+
+  return (
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+      <label htmlFor="title" className="block text-md font-bold mb-1">
+        Task:
+      </label>
+      <input
+        name="title"
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        value={task.title}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
+      />
+      <label htmlFor="description" className="block text-md font-bold mb-1">
+        Description:
+      </label>
+      <textarea
+        name="description"
+        placeholder="description"
+        onChange={handleChange}
+        value={task.description}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
+      ></textarea>
+      <button className="bg-indigo-600 px-2 py-1">Save</button>
+    </form>
+  );
+};
